7a78df0115ff247ee17803c9a424c158a7a3cf65
Abdul Hakeem <a.hakeem.y@gmail.com>
2018-04-16 19:28:18 +0800

    Tweaked View

--- bin/View.py
+++ bin/View.py
@@ -14,18 +14,18 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-font_name = 'Comic Sans MS'
-font_size = 18
+fontName = 'Comic Sans MS'
+fontSize = 18
 
-img_folder = os.path.join("asset")
+imgFolder = os.path.join("asset")
 
 
 class StaffTab(pygame.sprite.Sprite):
     def __init__(self, group=None):
         pygame.sprite.Sprite.__init__(self, group)
-        self.image = pygame.image.load(os.path.join(img_folder, "StaffTab.png")).convert()  # 384 x 216
+        self.image = pygame.image.load(os.path.join(imgFolder, "StaffTab.png")).convert()  # 384 x 216
         self.rect = self.image.get_rect()
-        self.rect.bottomright = (WIDTH * 26.5 // 100, HEIGHT)
+        self.rect.bottomRight = (WIDTH * 26.5 // 100, HEIGHT)
 
     def update(self):
         pass
@@ -34,9 +34,9 @@ class StaffTab(pygame.sprite.Sprite):
 class MenuTab(pygame.sprite.Sprite):
     def __init__(self, group=None):
         pygame.sprite.Sprite.__init__(self, group)
-        self.image = pygame.image.load(os.path.join(img_folder, "MenuTab.png")).convert()
+        self.image = pygame.image.load(os.path.join(imgFolder, "MenuTab.png")).convert()
         self.rect = self.image.get_rect()
-        self.rect.midbottom = (WIDTH * 45 // 100, HEIGHT)
+        self.rect.midBottom = (WIDTH * 45 // 100, HEIGHT)
 
     def update(self):
         pass
@@ -45,9 +45,9 @@ class MenuTab(pygame.sprite.Sprite):
 class InventoryTab(pygame.sprite.Sprite):
     def __init__(self, group=None):
         pygame.sprite.Sprite.__init__(self, group)
-        self.image = pygame.image.load(os.path.join(img_folder, "InventoryTab.png")).convert()
+        self.image = pygame.image.load(os.path.join(imgFolder, "InventoryTab.png")).convert()
         self.rect = self.image.get_rect()
-        self.rect.bottomleft = (WIDTH * 63.5 // 100, HEIGHT)
+        self.rect.bottomLeft = (WIDTH * 63.5 // 100, HEIGHT)
 
     def update(self):
         pass
@@ -56,15 +56,15 @@ class InventoryTab(pygame.sprite.Sprite):
 class FinanceTab(pygame.sprite.Sprite):
     def __init__(self, group=None):
         pygame.sprite.Sprite.__init__(self, group)
-        self.image = pygame.image.load(os.path.join(img_folder, "FinanceTab1.png")).convert()
+        self.image = pygame.image.load(os.path.join(imgFolder, "FinanceTab1.png")).convert()
         self.rect = self.image.get_rect()
-        self.rect.topleft = (WIDTH * 10 // 100, 0)
+        self.rect.topLeft = (WIDTH * 10 // 100, 0)
 
         self.text = "-1234567890"
-        self.font = pygame.font.SysFont(font_name, font_size)
-        self.textsurface = self.font.render(self.text, False, (0, 0, 0))
+        self.font = pygame.font.SysFont(fontName, fontSize)
+        self.textSurface = self.font.render(self.text, False, (0, 0, 0))
 
-        self.image.blit(self.textsurface, (80, 15))
+        self.image.blit(self.textSurface, (80, 15))
 
     def update(self):
         pass
@@ -73,15 +73,15 @@ class FinanceTab(pygame.sprite.Sprite):
 class CustomersTab(pygame.sprite.Sprite):
     def __init__(self, group=None):
         pygame.sprite.Sprite.__init__(self, group)
-        self.image = pygame.image.load(os.path.join(img_folder, "CustomerTab.png")).convert()
+        self.image = pygame.image.load(os.path.join(imgFolder, "CustomerTab.png")).convert()
         self.rect = self.image.get_rect()
-        self.rect.midtop = (WIDTH * 40 // 100, 0)
+        self.rect.midTop = (WIDTH * 40 // 100, 0)
 
         self.text = "50"
-        self.font = pygame.font.SysFont(font_name, font_size)
-        self.textsurface = self.font.render(self.text, False, (0, 0, 0))
+        self.font = pygame.font.SysFont(fontName, fontSize)
+        self.textSurface = self.font.render(self.text, False, (0, 0, 0))
 
-        self.image.blit(self.textsurface, (90, 18))
+        self.image.blit(self.textSurface, (90, 18))
 
     def update(self):
         pass
@@ -90,15 +90,15 @@ class CustomersTab(pygame.sprite.Sprite):
 class SatisfactionTab(pygame.sprite.Sprite):
     def __init__(self, group=None):
         pygame.sprite.Sprite.__init__(self, group)
-        self.image = pygame.image.load(os.path.join(img_folder, "SatisfactionTab.png")).convert()
+        self.image = pygame.image.load(os.path.join(imgFolder, "SatisfactionTab.png")).convert()
         self.rect = self.image.get_rect()
-        self.rect.topright = (WIDTH * 70 // 100, 0)
+        self.rect.topRight = (WIDTH * 70 // 100, 0)
 
         self.text = "Happy"
-        self.font = pygame.font.SysFont(font_name, font_size)
-        self.textsurface = self.font.render(self.text, False, (0, 0, 0))
+        self.font = pygame.font.SysFont(fontName, fontSize)
+        self.textSurface = self.font.render(self.text, False, (0, 0, 0))
 
-        self.image.blit(self.textsurface, (110, 15))
+        self.image.blit(self.textSurface, (110, 15))
 
 
 class DateTime(pygame.sprite.Sprite):
@@ -107,7 +107,7 @@ class DateTime(pygame.sprite.Sprite):
         self.image = pygame.Surface((WIDTH * 20 // 100, HEIGHT * 10 // 100))
         self.image.fill(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.topright = (WIDTH * 95 // 100, 0)
+        self.rect.topRight = (WIDTH * 95 // 100, 0)
 
 class PygameView:
     def __init__(self, evManager):
@@ -120,22 +120,22 @@ class PygameView:
         pygame.display.set_caption("Restaurant Simulator")
         self.clock = pygame.time.Clock()
 
-        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.allSprites = pygame.sprite.LayeredUpdates()
 
-        self.staffTab = StaffTab(self.all_sprites)
-        self.menuTab = MenuTab(self.all_sprites)
-        self.inventoryTab = InventoryTab(self.all_sprites)
-        self.financeTab = FinanceTab(self.all_sprites)
-        self.customersTab = CustomersTab(self.all_sprites)
-        self.satisfactionTab = SatisfactionTab(self.all_sprites)
-        self.datetime = DateTime(self.all_sprites)
+        self.staffTab = StaffTab(self.allSprites)
+        self.menuTab = MenuTab(self.allSprites)
+        self.inventoryTab = InventoryTab(self.allSprites)
+        self.financeTab = FinanceTab(self.allSprites)
+        self.customersTab = CustomersTab(self.allSprites)
+        self.satisfactionTab = SatisfactionTab(self.allSprites)
+        self.datetime = DateTime(self.allSprites)
 
 
     def Notify(self, event):
         if isinstance(event, TickEvent):
-            self.all_sprites.update()
+            self.allSprites.update()
 
             self.screen.fill(BLACK)
-            self.all_sprites.draw(self.screen)
+            self.allSprites.draw(self.screen)
 
             pygame.display.flip()
