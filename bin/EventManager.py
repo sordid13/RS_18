26f85ce5b19155e5333a2918512b80a18858cd52
Syameel <syameel09@gmail.com>
2018-05-06 02:51:15 +0800

    New Event Manager

--- bin/EventManager.py
+++ bin/EventManager.py
@@ -1,9 +1,12 @@
+from .Events import *
+
 class EventManager:
     # responsible for coordinating most communication
     # between the Model, View, and Controller
     def __init__(self):
         from weakref import WeakKeyDictionary
         self.listeners = WeakKeyDictionary()
+        self.eventQueue = []
 
     def RegisterListener(self, listener):
         self.listeners[listener] = 1
@@ -14,6 +17,16 @@ class EventManager:
 
     def Post(self, event):
         # Post a new event.  It will be broadcast to all listeners
-        keys = list(self.listeners.keys())
-        for listener in keys:
-            listener.Notify(event)
+        self.eventQueue.append(event)
+        if isinstance(event, TickEvent):
+            self.ConsumeEventQueue()
+
+    def ConsumeEventQueue(self):
+        for event in self.eventQueue:
+
+            keys = list(self.listeners.keys())
+            for listener in keys:
+                listener.Notify(event)
+
+        self.eventQueue = []
+
